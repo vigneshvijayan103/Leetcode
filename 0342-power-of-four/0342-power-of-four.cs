38844873e61a133955244d336313a4c37d797cfe
Vignesh V <vigneshvijayan103@gmail.com>
2025-07-07 10:05:34 +0530

    Time: 0 ms (100%), Space: 31 MB (55.31%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%4==0){
+            n=n/4;
+        }
+        return n==1;
+}
+}
