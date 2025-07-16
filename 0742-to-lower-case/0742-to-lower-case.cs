963a99648d2f53d688ec5b73006b4445b641b72f
Vignesh V <vigneshvijayan103@gmail.com>
2025-07-16 09:26:15 +0530

    Time: 16 ms (20.13%), Space: 42.3 MB (8.05%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string ToLowerCase(string s) {
+            // string empty="";
+            // foreach(i in s)[
+            //     if(char.IsUpper(i))
+                return s.ToLower();
+            
+    }
+}
