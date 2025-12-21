654f6179f85e85d9de627127a5afb4248c975891
Vignesh V <vigneshvijayan103@gmail.com>
2025-12-21 15:00:45 +0530

    Time: 30 ms (27.23%), Space: 48.1 MB (98.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,16 +1,18 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        int[]outs=new int[2];
-        for(int i=0;i<nums.Length-1;i++){
-            for(int j=i+1;j<nums.Length;j++){
-                if(nums[i]+nums[j]==target){
-                     outs[0]=i;
-                     outs[1]=j;
-                     return outs;
-                }
+        int[]arr=new int[2];
+        for(int i=0;i<nums.Length;i++){
+          for(int j=i+1;j<nums.Length;j++){
+            if(nums[i]+nums[j]==target){
+                arr[0]=i;
+                arr[1]=j;
+                return arr;
             }
+          }
         }
-        return outs;
-    }
 
+    return arr;
+
+
+    }
 }
