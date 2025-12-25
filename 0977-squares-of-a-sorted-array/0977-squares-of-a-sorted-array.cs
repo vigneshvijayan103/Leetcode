a0d60911dde73e87d52d249d14079f08bde496e0
Vignesh V <vigneshvijayan103@gmail.com>
2025-12-25 13:31:41 +0530

    Time: 376 ms (5.09%), Space: 64.9 MB (41.74%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
@@ -3,7 +3,14 @@ public class Solution {
         for(int i=0;i<nums.Length;i++){
             nums[i]=nums[i]*nums[i];
         }
-        Array.Sort(nums);
+        for(int j=0;j<nums.Length;j++){
+            for(int k=j+1;k<nums.Length;k++){
+                if(nums[j]>nums[k]){
+                    int temp=nums[j];
+                    nums[j]=nums[k];
+                    nums[k]=temp;
+                }
+            }        }
         
         return nums;
     }
