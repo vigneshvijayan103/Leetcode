b8492f2fe291bfc0cff85eca814e87a3e198a69a
Vignesh V <vigneshvijayan103@gmail.com>
2026-02-04 18:15:31 +0530

    Time: 1737 ms (5.09%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
-select patient_id ,patient_name,conditions 
-from Patients  
-WHERE ' ' + conditions + ' ' LIKE '% DIAB1%';
+select 
+* 
+from patients 
+where conditions like '% DIAB1%' or conditions like 'DIAB1%'
