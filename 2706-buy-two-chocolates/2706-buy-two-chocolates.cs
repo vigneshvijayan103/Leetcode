613d323d076a273db8abc7dec96ac1dff83df558
Vignesh V <vigneshvijayan103@gmail.com>
2026-02-04 15:37:01 +0530

    Time: 4 ms (17.07%), Space: 47.9 MB (70.73%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cs
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cs
@@ -1,22 +1,15 @@
 public class Solution {
     public int BuyChoco(int[] prices, int money) {
         int number=money;
-        int buy=0;
       Array.Sort(prices);
-        for(int i=0;i<prices.Length;i++){
-            if( buy<2 && prices[i]<=number && number>0){
-                number-=prices[i];
-                Console.WriteLine(number);
-                buy++;
-            }
-            
-        }
-        if(buy==2){
-            return number;
-        }
-        else{
+        int sumOfTwo=prices[0]+prices[1];
+        if(sumOfTwo<=money)
+              number-=sumOfTwo;
+              return number;
             return money;
-        }
+       
+
+
         
     }
 }
