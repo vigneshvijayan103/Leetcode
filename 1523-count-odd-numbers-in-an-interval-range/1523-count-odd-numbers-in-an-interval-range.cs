b3ce1b570b8e6c93f50b0c4d7ebe4b4659deb510
Vignesh V <vigneshvijayan103@gmail.com>
2026-02-05 09:42:48 +0530

    Time: 2232 ms (8.76%), Space: 29.3 MB (22.1%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int CountOdds(int low, int high) {
+        int count=0;
+        for(int i=low;i<=high;i++){
+            if(i%2!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
