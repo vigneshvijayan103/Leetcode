d39f59172ec60a00999cdf17bcec68207d2c66d8
Vignesh V <vigneshvijayan103@gmail.com>
2026-02-06 10:48:23 +0530

    Time: 1 ms (26.95%), Space: 40.4 MB (6.49%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int NumJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(int i=0;i<jewels.Length;i++){
+            for(int j=0;j<stones.Length;j++){
+                if(jewels[i]==stones[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+       
+        
+    }
+}
