3e080555c0aa6ac7a91617ef27cce9dc5237f411
Vignesh V <vigneshvijayan103@gmail.com>
2026-02-18 12:45:05 +0530

    Time: 20 ms (6.17%), Space: 44.6 MB (6.17%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cs
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cs
@@ -4,11 +4,8 @@ public class Solution {
         int sum=0;
         for(int i=0;i<s.Length;i++){
             int rev=char.ToUpper(s[i]);
-          int reverse='Z'-char.ToUpper(s[i])+1;
-          Console.WriteLine(reverse);
-          int product=(i+1)*reverse;
-          Console.WriteLine("pro"+product);
-          sum+=product;
+          int reverse=26-(s[i]-'a');
+          sum+=reverse*(i+1);
         }
         return sum;
     }
